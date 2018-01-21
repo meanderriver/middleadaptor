f6e7d0513ca3d1f095f7da9bb90129dbc3c97b06
SDestiny233 <31030883+meanderriver@users.noreply.github.com>
2018-01-21 17:04:04 +0800

    update

--- ma-cache/src/main/java/org/ma/cache/App.java
+++ ma-cache/src/main/java/org/ma/cache/App.java
@@ -1,4 +1,4 @@
-package org.middleware.middleadaptor;
+package org.ma.cache;
 
 /**
  * Hello world!
--- ma-cache/src/test/java/org/ma/cache/AppTest.java
+++ ma-cache/src/test/java/org/ma/cache/AppTest.java
@@ -1,4 +1,4 @@
-package org.middleware.middleadaptor;
+package org.ma.cache;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
