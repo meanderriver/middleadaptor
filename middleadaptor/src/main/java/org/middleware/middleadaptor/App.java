49f0c4722f1fbbba82fe5300087661c76521a979
SDestiny233 <31030883+meanderriver@users.noreply.github.com>
2018-01-21 15:31:33 +0800

    md
    initial

---
+++ middleadaptor/src/main/java/org/middleware/middleadaptor/App.java
@@ -0,0 +1,13 @@
+package org.middleware.middleadaptor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
